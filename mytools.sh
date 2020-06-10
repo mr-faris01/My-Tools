ee252b58925424f276d28cd043fa0d578bfbbf3d
mr-faris01 <66506246+mr-faris01@users.noreply.github.com>
2020-06-10 11:36:44 +0700

    Add files via upload

---
+++ mytools.sh
@@ -0,0 +1,228 @@
+#ngodingskuy
+clear
+
+figlet -f slant "Mr-Faris" | lolcat
+echo "\33[32;1mX====================================================X"
+echo "\33[36;1m Author: Mr-Faris01"
+echo "\33[36;1m Github: https://github.com/mr-faris01"
+echo "\33[36;1m You tube: Faris GamingID"
+echo "\33[36;1m Kontak : 0895611036046"
+echo "\33[32;1mX====================================================X"
+sleep 1
+echo "\33[37;1mPilihan Menu"
+echo "\33[31;1m[=============================================]"
+echo "\33[37;1m[01] Tools Spam"
+echo "\33[1;33m[=============================================|"
+echo "\33[37;1m[02] Hack FB dark-f1"
+echo "\33[32;1m[=============================================]"
+echo "\33[37;1m[03] Virtex Untuk WA"
+echo "\33[0;36m[=============================================]"
+echo "\33[37;1m[04] Hack IG"
+echo "\33[31;1m[=============================================]"
+echo "\33[37;1m[05] Red Hawk"
+echo "\33[1;33m[=============================================]"
+echo "\33[37;1m[06] Membuat Virus"
+echo "\33[32;1m[=============================================]"
+echo "\33[37;1m[07] Tools B4J1N64N"
+echo "\33[0;36m[=============================================]"
+echo "\33[37;1m[08] Script MBF"
+echo "\33[31;1m[=============================================]"
+echo "\33[37;1m[09] Tools Komen FB"
+echo "\33[1;33m[=============================================]"
+echo "\33[37;1m[10] Ddos BPC"
+echo "\33[32;1m[=============================================]"
+echo "[00] Keluar"
+read -p "[?] pilih :" pil;
+
+# batas gan
+if [ $pil = 01 ]
+then
+	echo "\33[32;1mTools Spam"
+	sleep 2
+	pkg update && pkg upgrade
+	pkg install git
+	pkg install python
+	git clone https://github.com/Abinayanafaiq/TOOLS5TX
+	cd TOOLS5TX
+	pip install -r requirements.txt
+	python TOOLS5TX
+	sleep 2
+	echo "\33[32;1mDONE INSTALL"
+	sleep 2
+	echo "\33[32;1mketik: cd TOOLS5TX"
+	sleep 2
+	echo "\33[32;1mketik lagi: python TOOLS5TX"
+fi
+
+if [ $pil = 02 ]
+then
+	echo "\33[32;1mScript Termux Lengkap"
+	sleep 2
+	git clone https://github.com/mr-faris01/Dark-F1
+	sleep 2
+	echo "\33[32;1mDONE INSTALL"
+	sleep 2
+	echo "\33[32;1mketik: cd dark-f1"
+	sleep 2
+	echo "\33[32;1mketik lagi: python2 dark-f1.py"
+	sleep 2
+	echo "\33[32;1mUsername: faris"
+	sleep 2
+	echo "\33[32;1mketik lagi: lana01"
+fi
+
+if [ $pil = 03 ]
+then
+	echo "\33[32;1mVirtex untuk WA"
+	sleep 2
+	apt install git
+	apt install curl
+	apt install figlet
+	apt install ruby
+	gem install lolcat
+	git clone https://github.com/muhammadfathul/VIRTEX
+	chmod +x virtex.sh
+	sleep 2
+	echo "\33[32;1mDONE INSTALL"
+	sleep 2
+	echo "\33[32;1mketik: cd VIRTEX"
+	sleep 2
+	echo "\33[32;1mketik lagi: sh virtex.sh"
+fi
+
+if [ $pil = 04 ]
+then
+	echo "\33[1;33mHack Fb IG"
+	sleep 2
+	pkg install nano
+	pkg install figlet 
+	pkg install python2
+	pip2 install lolcat 
+	pkg install cowsay
+	pkg install git
+	git clone https://github.com/darkcurut08/darkcurut08
+	sleep 2
+	echo "\33[1;33mDONE INSTALL"
+	sleep 2
+	echo "\33[1;33mketik: cd darkcurut08"
+	sleep 2
+	echo "\33[1;33mketik lagi: sh Moreno77.sh"
+fi
+
+
+if [ $pil = 05 ]
+then
+	echo "\33[1;33mRed Hawk"
+	sleep 2
+	git clone https://github.com/Tuhinshubhra/RED_HAWK
+	chmod +x rhawk.php
+	sleep 2
+	echo "\33[1;33mDONE INSTALL"
+	sleep 2
+        echo "\33[1;33mketik: cd RED_HAWK"
+	sleep 2
+        echo "\33[1;33mketik lagi: php rhawk.php"
+fi
+
+
+if [ $pil = 06 ]
+then
+	echo "\33[1;33mMembuat Virus"
+	sleep 2
+	pkg update 
+	pkg upgrade
+	pkg install python2
+	pkg install git
+	git clone https://github.com/Mr-xDODOL/Malicious
+	cd Malicious
+	pip2 install -r requirements.txt
+	pip2 install tqdm
+	pip2 install requests
+	chmod 777 malicious.py
+	sleep 2
+	echo "\33[1;33mDONE INSTALL"
+	sleep 2
+        echo "\33[1;33mketik: unzip Malicious jika belum menginstall nya kalu udah pernah nginstall gak usah lagi"
+	sleep 2
+        echo "\33[1;33mketik lagi: cd Malicious"
+	sleep 2
+	echo "\33[1;33mketik lagi: python2 malicious.py"
+	sleep 2
+	echo "\33[1;33mjika sudah selesai maka pindahkan virus ke memory internal"
+	sleep 2
+	echo "\33[1;33mketik: ls"
+	sleep 2
+	echo "\33[1;33mketik lagi: cd Android"
+	sleep 2
+	echo "\33[1;33mketik lagi: mv nama virus /sdcard"
+fi
+
+
+if [ $pil = 07 ]
+then
+	echo "\33[1;33mTools B4J1N64N"
+	sleep 2
+	git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+	sleep 2
+	echo "\33[1;33mDONE INSTALL"
+	sleep 2
+        echo "\33[1;33mketik: cd B4J1N64Nv5"
+	sleep 2
+        echo "\33[1;33mketik lagi: sh B4J1N64N.sh"
+fi
+
+
+if [ $pil = 08 ]
+then
+	echo "\33[1;33mScript MBF"
+	sleep 2
+	git clonehttps://github.com/pirmansx/mbf
+	pip2 install mechanize
+	sleep 2
+	echo "\33[1;33mDONE INSTALL"
+	sleep 2
+        echo "\33[1;33mketik: cd mbf"
+	sleep 2
+        echo "\33[1;33mketik lagi: python2 MBF.py"
+fi
+
+
+if [ $pil = 09 ]
+then
+	echo "\33[1;33mKomen FB"
+	sleep 2
+	git clone https://github.com/arief125/komenfb
+	sleep 2
+	echo "\33[1;33mDONE INSTALL"
+	sleep 2
+        echo "\33[1;33mketik: cd komenfb"
+	sleep 2
+        echo "\33[1;33mketik lagi: python2 komenfb.py"
+fi
+
+
+if [ $pil = 10 ]
+then
+	echo "\33[1;33mDdos BPC"
+	sleep 2
+	git clone http://github.com/BLACK-PHANTOM-CYBER/DDOS-BPC
+	sleep 2
+	echo "\33[1;33mDONE INSTALL"
+	sleep 2
+        echo "\33[1;33mketik: cd DDOS-BPC"
+	sleep 2
+        echo "\33[1;33mketik lagi: python2 SPOONDDOS.sh"
+fi
+
+
+if [ $pil = 00 ]
+then
+	sleep 2
+	echo "\33[1;33mKELUAR DARI TOOLS"
+	sleep 2
+	echo "\33[1;33mTERIMAKASI SUDAH MENGGUNAKAN TOOLS INI"
+	sleep 2
+	echo "\33[1;33mJIKA TERJADI ERROR CHAT WA SAYA DI ATAS"
+	sleep 2
+	echo "\33[1;33mSALAM JOMBLO BY"
+fi
